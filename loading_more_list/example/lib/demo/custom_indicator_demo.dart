d162d638edd59f554ab889d1260f8bd1f2976666
zhoumaotuo <zmtzawqlp@live.com>
2018-12-10 13:16:43 +0800

    corret custom indicator demo

--- loading_more_list/example/lib/demo/custom_indicator_demo.dart
+++ loading_more_list/example/lib/demo/custom_indicator_demo.dart
@@ -14,6 +14,7 @@ class CustomIndicatorDemo extends StatefulWidget {
 
 class _CustomIndicatorDemoState extends State<CustomIndicatorDemo> {
   TuChongRepository listSourceRepository;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -39,9 +40,9 @@ class _CustomIndicatorDemoState extends State<CustomIndicatorDemo> {
           Expanded(
             child: LoadingMoreList(
               ListConfig<TuChongItem>(
-                  ItemBuilder.itemBuilder, listSourceRepository,
-                  indicatorBuilder: _buildIndicator,
-                  padding: EdgeInsets.all(0.0),
+                ItemBuilder.itemBuilder, listSourceRepository,
+                indicatorBuilder: _buildIndicator,
+                padding: EdgeInsets.all(0.0),
               ),
             ),
           )
@@ -53,79 +54,138 @@ class _CustomIndicatorDemoState extends State<CustomIndicatorDemo> {
   //you can use IndicatorWidget or build yourself widget
   //in this demo, we define all status.
   Widget _buildIndicator(BuildContext context, IndicatorStatus status) {
+    bool isSliver=false;
     Widget widget;
-    bool full = (status == IndicatorStatus.FullScreenBusying || status ==IndicatorStatus.FullScreenError);
+    bool full = (status == IndicatorStatus.FullScreenBusying ||
+        status == IndicatorStatus.FullScreenError);
     double height = 35.0;
     switch (status) {
       case IndicatorStatus.None:
         widget = Container(height: 0.0);
-        height = 0.0;
         break;
       case IndicatorStatus.LoadingMoreBusying:
-      case IndicatorStatus.FullScreenBusying:
-        double indicatorSize = full ? 30.0 : 15.0;
         widget = Row(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Container(
-                margin: EdgeInsets.only(right: 15.0),
-                height: indicatorSize,
-                width: indicatorSize,
-                child: getIndicator(context)),
-            (!full
-                ? Text(
-                    "正在加载...慌什么慌",
-                  )
-                : Text("正在加载...慌什么慌",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 28.0))),
+              margin: EdgeInsets.only(right: 5.0),
+              height: 15.0,
+              width: 15.0,
+              child: getIndicator(context),
+            ),
+            Text("正在加载...稍等一下")
           ],
         );
+        widget = _setbackground(false, widget, 35.0);
+        break;
+      case IndicatorStatus.FullScreenBusying:
+        widget = Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(right:10.0),
+              height: 30.0,
+              width: 30.0,
+              child: getIndicator(context),
+            ),
+            Text("正在加载...稍等一下")
+          ],
+        );
+        widget = _setbackground(true, widget, double.infinity);
+        if (isSliver) {
+          widget = SliverFillRemaining(
+            child: widget,
+          );
+        }
+//        else {
+//          widget = SingleChildScrollView(
+        //  child: widget,
+        // );
+        //}
         break;
       case IndicatorStatus.Error:
+        widget = Text(
+           "加载失败，点击重试",
+        );
+        widget = _setbackground(false, widget, 35.0);
+
+          widget = GestureDetector(
+            onTap: () {
+              listSourceRepository.errorRefresh();
+            },
+            child: widget,
+          );
+
+        break;
       case IndicatorStatus.FullScreenError:
         widget = Text(
-          "加载失败，搞个川川",
+          "加载失败，点击重试",
         );
+        widget = _setbackground(true, widget, double.infinity);
+          widget = GestureDetector(
+            onTap: () {
+              listSourceRepository.errorRefresh();
+            },
+            child: widget,
+          );
+        if (isSliver) {
+          widget = SliverFillRemaining(
+            child: widget,
+          );
+        }
+//        else {
+//          widget = SingleChildScrollView(
+//            child: widget,
+//          );
+//        }
         break;
       case IndicatorStatus.NoMoreLoad:
-        widget = Text("没有了，不要拖了");
+        widget = Text("没有更多了，不要拖了。。。");
+        widget = _setbackground(false, widget, 35.0);
         break;
       case IndicatorStatus.Empty:
         widget = EmptyWidget(
           "这里只有空",
         );
+        widget = _setbackground(true, widget, double.infinity);
+        if (isSliver) {
+          widget = SliverToBoxAdapter(
+            child: widget,
+          );
+        }
+        //else {
+//          widget = SingleChildScrollView(
+//            child: widget,
+//          );
+//        }
         break;
-    }
-
-    widget = Container(
-        width: double.infinity,
-        height: full ? double.infinity : height,
-        child: widget,
-        color: Colors.grey[200],
-        alignment: Alignment.center);
-
-    if (status == IndicatorStatus.Error || status ==IndicatorStatus.FullScreenError) {
-      widget = GestureDetector(
-        onTap: () {
-          listSourceRepository.errorRefresh();
-        },
-        child: widget,
-      );
     }
     return widget;
   }
-}
 
-Widget getIndicator(BuildContext context) {
-  return Platform.isIOS
-      ? CupertinoActivityIndicator(
-    animating: true,
-    radius: 16.0,
-  )
-      : CircularProgressIndicator(
-    strokeWidth: 2.0,
-    valueColor: AlwaysStoppedAnimation(Colors.red),
-  );
+  Widget _setbackground(bool full, Widget widget, double height) {
+    widget = Container(
+        width: double.infinity,
+        height: height,
+        child: widget,
+        color: Colors.grey[200],
+        alignment: Alignment.center);
+    return widget;
+  }
+
+  Widget getIndicator(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoActivityIndicator(
+      animating: true,
+      radius: 16.0,
+    )
+        : CircularProgressIndicator(
+      strokeWidth: 2.0,
+      valueColor: AlwaysStoppedAnimation(Theme
+          .of(context)
+          .primaryColor),
+    );
+  }
 }
