373d48b524d49eacaf1bdd995e968bc434f79d7c
zhoumaotuo <zmtzawqlp@live.com>
2019-02-14 23:15:00 +0800

    [move project to new github](https://github.com/fluttercandies/extended_network_image)

--- extended_network_image/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-[0.1.0]
-
-* TODO: Describe initial release.
--- extended_network_image/lib/extended_network_image.dart
+++
@@ -1,6 +0,0 @@
-library extended_network_image;
-
-export 'src/extended_network_image.dart';
-export 'src/extended_network_image_load_state_widget.dart';
-export 'src/extended_network_image_provider.dart';
-export 'src/extended_network_image_utils.dart';
