588074252d055d4dc00416f5bd9a132e8e867860
zhoumaotuo <zmtzawqlp@live.com>
2019-02-14 23:23:06 +0800

    [move project to new github](https://github.com/fluttercandies/http_client_helper)

--- http_client_helper/lib/http_client_helper.dart
+++
@@ -1,7 +0,0 @@
-//A Flutter plugin for http request with cancel and retry fuctions.
-
-library http_client_helper;
-
-export 'src/cancellation_token.dart';
-export 'src/http_client_helper.dart';
-export 'src/retry_helper.dart';
