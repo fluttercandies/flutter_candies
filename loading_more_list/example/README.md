dac095d55fa90c6e5a8481ffaba6efacd7b423a1
zhoumaotuo <zmtzawqlp@live.com>
2019-02-14 23:34:04 +0800

    [move project to new github](https://github.com/fluttercandies/loading_more_list)

--- loading_more_list/example/README.md
+++
@@ -1,8 +0,0 @@
-# example
-
-A new Flutter application.
-
-## Getting Started
-
-For help getting started with Flutter, view our online
-[documentation](https://flutter.io/).
--- loading_more_list/lib/src/refresh_base.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:async';
-
-abstract class RefreshBase {
-  //if clear is true, it will clear list,before request
-  Future<bool> refresh([bool clearBeforeRequest = false]);
-  Future<bool> errorRefresh();
-}
